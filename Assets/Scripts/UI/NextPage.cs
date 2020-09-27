42efe30ac5f98f96dc9e8117c69749aa75ac82d2
kossam001 <kossam001@gmail.com>
2020-09-26 21:32:20 -0400

    Resized Instructions and Added Script to Turn Pages in Instructions

---
+++ Assets/Scripts/UI/NextPage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPage : MonoBehaviour
+{
+    public List<GameObject> pages;
+    private int pageNum = 0;
+
+    public void TurnPage()
+    {
+        pages[pageNum].SetActive(false);
+
+        if (pageNum == pages.Count - 1)
+        {
+            pageNum = 0;
+        }
+        else
+        {
+            pageNum++;
+        }
+
+        pages[pageNum].SetActive(true);
+    }
+}
