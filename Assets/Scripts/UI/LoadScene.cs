dafed3b6d9fbfc2395b428ffa4fa1117eb955e72
kossam001 <kossam001@gmail.com>
2020-09-29 14:29:01 -0400

    Separated Each Screen to be Its Own Scene

---
+++ Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿/* LoadScene.cs
+ * 
+ * Samuel Ko
+ * 101168049
+ * Last Modified: 2020-09-29
+ * 
+ * Loads the scene with sceneName when onButtonClicked is called.
+ * 
+ * 2020-09-29: Added this script.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneName;
+
+    public void onButtonClicked()
+    {
+        Debug.Log("Button Clicked");
+        SceneManager.LoadScene(sceneName);
+    }
+}
