20d8fd414683c55545f4ca159ce89af9a0295824
kossam001 <kossam001@gmail.com>
2020-10-06 21:04:55 -0400

    Added a Script to Allow Camera to Follow Player

---
+++ Assets/Scripts/Functional/Follow.cs
@@ -0,0 +1,34 @@
+﻿/* Follow.cs
+ * 
+ * Samuel Ko
+ * 101168049
+ * Last Modified: 2020-10-06
+ * 
+ * GameObject with this script will follow the target.  
+ * Used to copy the position of another GameObject without parenting them.
+ * Since it is intended to be used with the camera, the z value should not be
+ * copied.
+ * 
+ * 2020-10-06: Added this script.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    }
+}
